83377d5be15ee9b276a0480163fc88170a7b0765
dav1d <dmedina5861@bths.edu>
2016-11-15 23:45:41 -0500

    Finished procedures to set and print the grid.

--- Detective/src/DavidAndKristy/DavidDotsBoxes.java
+++ Detective/src/DavidAndKristy/DavidDotsBoxes.java
@@ -6,23 +6,54 @@ public class DavidDotsBoxes {
 
 	public static Scanner in = new Scanner(System.in);
 	public static int inputInt;
+	public static String[][] grid;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Enter number of rows between 3 and 9.");
 		int row = in.nextInt();
 		System.out.println("Enter number of columns between 3 and 9.");
 		int col = in.nextInt();
-		printBox(row,col);
+		setGrid(row,col); // setter.
+		printGrid(); // printer.
 
 	}
-	public static void printBox(int row, int col){
+	public static void setGrid(int row, int col){
 		// to connect two points, we'll use 'o--' instead of 'o', IF horizontal.
-		for(int y = 0; y <= row; ++y){
-			for(int x = 0; x <= col; ++x){
-				System.out.print("o--");
+		grid = new String[(2 * (row + 1)) - 1][col + 1];
+		String token; // string that will be given to column.
+		for(int y = 0; y < grid.length; ++y){ // traverse through the rows.
+			/*
+			*  When creating lines, there are Vertical and Horizontal lines.
+			*  Horizontal lines will be easy to make and are nothing special.
+			*  But vertical lines are different, since they require a String Line of its.
+			*  We will draw both lines by dividing the rows by two types.
+			*
+			*  The rows will be divided into two types -> even or odd.
+			*  Even -> the dots will be drawn.
+			*  Odd -> space will be allocated for future VERTICAL lines.
+			*
+			*  A token will be used to give the column the value needed.
+			*  token -> if even: regular row with dot. if odd: special row that will be used to allocate space.
+			* */
+			if(y % 2 == 0) token = "o  "; // even row.
+			else token = ""; // odd row.
+			for(int x = 0; x < grid[y].length; ++x){ // traverse through the column.
+				grid[y][x] = token; // set the value to the token based on the type of row.
 			}
-			System.out.println("");
-		}//
+		}
+	}
+	public static void printGrid(){
+		// procedure to print the grid array.
+		for(int y = 0; y < grid.length; ++y){
+			for(int x = 0; x < grid[y].length; ++x){
+				System.out.print(grid[y][x]);
+			}
+			System.out.println(""); // every row needs a new line.
+		}
+	}
+	public static void setVertLn(int x1, int y1, int x2, int y2){
+
+		
 	}
 
 }

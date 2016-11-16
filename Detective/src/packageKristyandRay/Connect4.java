ffa0ee4fcd5fc42335b6103e6c6cd2c6b8b83a2d
Ray <Ray@192.168.1.107>
2016-11-15 22:07:00 -0500

    Layout of connect4 event started

--- Detective/src/packageKristyandRay/Connect4.java
+++ Detective/src/packageKristyandRay/Connect4.java
@@ -2,5 +2,11 @@ package packageKristyandRay;
 
 import java.util.Scanner;
 
-public class Connect4 {
+public class Connect4 implements caveExplorer.Event{
+	private static String[] storyPart1 = {};
+	@Override
+	public void play() {
+		
+	}
+	
 }

e0e74631da59e056f99cf341512e6402cb794f61
Kristy <Kristy@kristys-mbp.home>
2016-11-19 01:07:52 -0500

    computerAI; checkBoxes next

--- Detective/src/DavidAndKristy/KristyAI.java
+++ Detective/src/DavidAndKristy/KristyAI.java
@@ -11,74 +11,48 @@ public class KristyAI {
 	
 	public static void main(String[] args){
 		setGrid(3,3);
-		//printGrid();
 		//checkBoxes();
-		computerTurn();
+		grid[0][0] = "o--";	
+		grid[1][0] = "|  ";
+		grid[1][1] = "|  ";
+		checkBoxes();
+		printGrid();
+		//computerTurn();
+		
 	}
 	
 	public static void computerTurn(){
 		boolean inLoop = true;
 		
 		while(inLoop){
-			x = (int) (Math.random() * 4); // is not random; need to fix while loop
+			checkBoxes();
+			x = (int) (Math.random() * 7); // is not random; need to fix while loop
 			y = (int) (Math.random() * 4);
-			int vh = (int) (Math.random()* 2); //0 is vertical; 1 is horizontal
-			if(vh == 0){
-				if(grid[x][y].equals("")){
-					grid[x][y] = "|  ";
-					printGrid();
-					inLoop = false;
-				}
+			System.out.println("x = " + x);
+			System.out.println("y = " + y);
+			if(x % 2 == 0 && grid[x][y] != "o--" && y != 3){
+				grid[x][y] = "o--";
+				printGrid();
+				inLoop = false;
 			}
-			else if(vh == 1){
-				if(grid[x][y] == "o  "){
-					grid[x][y] = "o--";
-					printGrid();
-					inLoop = false;
-				}
+			else if(x % 2 == 1 && grid[x][y] != "|  "){
+				grid[x][y] = "|  ";
+				printGrid();
+				inLoop = false;
 			}
-			if(inLoop == false){
-				inLoop = true;
-			}
-			
-//			if(grid[x][y] == "o  " || grid[x][y] == ""){ //change to .equals
-//				if(x!= 2 && grid[x][y] == "o  "){
-//					grid[x][y] = "o--";
-//				}
-//				else if(grid[x][y] == ""){
-//					grid[x][y] = "|";
-//				}
-//				printGrid();
-//				inLoop = false;
-//			}
-//			else{
-//				inLoop = true;
-//			}
 		}
 	}
 	
 	public static void checkBoxes(){
-		int count = 0;
-		int[] checked = new int[3];
 		for(int row = 0; row < grid.length; row++){
 			for(int col = 0; col < grid[0].length; col++){
-				if(grid[row][col] == "o" || grid[row][col] == "==="){
-					count++; 
-					if(grid[row + 1][col] == "o--" || grid[row - 1][col] == "o--"){
-						count++;
-					}
-					if(grid[row][col + 1] == "|"){
-						count++;
-					} 
-					if(grid[row][col - 1] == "|"){
-						count++;
-					} 
+				if(grid[row][col].equals("o--") && grid[row + 1][col].equals("|") && grid[row + 2][col].equals("o--")){
+					grid[row + 1][col + 1] = "|  ";
+					System.out.println("y");
 				}
-				if(count == 3){
-					//needs to check for vertical or horizontal
-					grid[row][col] = "o--";
-					count = 0;
-					break;
+				else if(grid[row][col].equals("o--") && grid[row + 1][col].equals("|") && grid[row + 1][col + 1].equals("|")){
+					grid[row + 2][col] = "o---";
+					System.out.println("n");
 				}
 			}
 		}
@@ -103,7 +77,7 @@ public class KristyAI {
 			*  token -> if even: regular row with dot. if odd: special row that will be used to allocate space.
 			* */
 			if(y % 2 == 0) token = "o  "; // even row.
-			else token = ""; // odd row.
+			else token = "   "; // odd row.
 			for(int x = 0; x < grid[y].length; ++x){ // traverse through the column.
 				grid[y][x] = token; // set the value to the token based on the type of row.
 			}

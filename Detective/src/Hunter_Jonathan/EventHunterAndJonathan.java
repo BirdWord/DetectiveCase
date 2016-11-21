6d66c2527fe5a2ef62fd260691294d7d6e42c1c1
Student 6 <Student 6@10.8.33.102>
2016-11-21 12:31:54 -0500

    pushing stuff

--- Detective/src/Hunter_Jonathan/EventHunterAndJonathan.java
+++ Detective/src/Hunter_Jonathan/EventHunterAndJonathan.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import caveExplorer.CaveExplorer;
 import caveExplorer.CaveRoom;
 
-public class EventHunterAndJonathan {
+public class EventHunterAndJonathan implements caveExplorer.Event{
 
 	//radars are odd
 	//ships are even
@@ -13,7 +13,7 @@ public class EventHunterAndJonathan {
 	public static String[][] AIboard1;
 	public static String[][] board2;
 	public static String[][] AIboard2;
-	private static String map;
+	public static String map;
 	
 	public EventHunterAndJonathan() {
 		Scanner input = new Scanner(System.in);
@@ -21,69 +21,11 @@ public class EventHunterAndJonathan {
 		AIboard1 = new String[6][6];
 		board2 = new String[6][6];
 		AIboard2 = new String[6][6];
-		int shotsHit = 0;
-		int tries = 0;
+	}
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
 		
-		System.out.println("Welcome to the game of Battleship.");
-		//fix to make it print both boards
-		print(board1.length, board1[0].length);
-		placeShip();
-		if(JonathanBattleShip.win() == false){
-			while(!JonathanBattleShip.win()){
-				print(board1.length, board1[0].length);
-				tries++;
-				if(input.equals("ship")){
-					System.out.println("You have won the game.");
-				}
-				System.out.println("Which row would you like to hit?");
-				int x = input.nextInt();
-				System.out.println("Which column would you like to hit?");
-				int y = input.nextInt();
-				JonathanBattleShip.attack(x,y);
-				if(JonathanBattleShip.hit() == true){
-					shotsHit++;
-				}
-				if(JonathanBattleShip.AIwin() == true){
-					System.out.println("You have lost the game. You are unable to solve the mystery and the murderer has gotten away.");
-					break;
-				
-				}
-			}
-			System.out.println("You have won the game. It has taken you" + tries + "to beat the game.");
-			System.out.println("Here is your last clue.");
-		}
 	}
-
-	 private void placeShip() {
-		 Scanner input = new Scanner(System.in);
-		 System.out.println("Where would you like to place your first ship?");
-			int x = input.nextInt();
-			int y = input.nextInt();
-			board1[x][y] = " O ";
-			System.out.println("Where would you like to place your second ship?");
-			int x1 = input.nextInt();
-			int y1 = input.nextInt();
-			board1[x1][y1] = " O ";
-			System.out.println("Where would you like to place your third ship?");
-			int x2 = input.nextInt();
-			int y2 = input.nextInt();
-			board1[x2][y2] = " O ";
-	}
-
-	 public static void print(int roomsX, int roomsY) {
-		 map = " ";
-			for(int i = 0; i < board1[0].length + 1;i++){
-				map += "___";//4 underscores
-			}
-			map += "___\n";//3 underscores
-			for(CaveRoom[] row: CaveExplorer.caves){
-				//three rows of text
-				for(int i = 0; i < 3; i++){
-					//a line of text for each
-					String text = "";
-					text += "|";
-					map += text + "\n";
-				}// last of the 3 text lines
-			}//last row
-}
 }
--- Detective/src/Hunter_Jonathan/JonathanBattleShip.java
+++ Detective/src/Hunter_Jonathan/JonathanBattleShip.java
@@ -2,8 +2,44 @@ package Hunter_Jonathan;
 
 import java.util.Scanner;
 
+import caveExplorer.CaveExplorer;
+import caveExplorer.CaveRoom;
+
 public class JonathanBattleShip {
 	static int numberOfPlayerShips = 3;
+	
+	public static void startGame(){
+		Scanner input = new Scanner(System.in);
+		int shotsHit = 0;
+		int tries = 0;
+		
+		System.out.println("Welcome to the game of Battleship.");
+		//fix to make it print both boards
+		//print(EventHunterAndJonathan.board1.length, EventHunterAndJonathan.board1[0].length);
+		placeShip();
+		if(JonathanBattleShip.win() == false){
+			while(!JonathanBattleShip.win()){
+				//print(EventHunterAndJonathan.board1.length, EventHunterAndJonathan.board1[0].length);
+				tries++;
+				if(input.equals("ship")){
+					System.out.println("You have won the game.");
+				}
+				if(JonathanBattleShip.hit() == true){
+					System.out.println("You have hit a ship.");
+					shotsHit++;
+				}else{
+					System.out.println("You did not hit a ship.");
+				}
+				if(JonathanBattleShip.AIwin() == true){
+					System.out.println("You have lost the game. You are unable to solve the mystery and the murderer has gotten away.");
+					break;
+				
+				}
+			}
+			System.out.println("You have won the game. It has taken you" + tries + "to beat the game.");
+			System.out.println("Here is your last clue.");
+		}
+	}
 	
 	public static boolean win(){
 		int shotsHit = 0;
@@ -13,17 +49,26 @@ public class JonathanBattleShip {
 		return false;
 	}
 
-	public static void attack(int x2, int y2){
+	public static void attack(){
+		Scanner input = new Scanner(System.in);
+		System.out.println("Which row would you like to hit?");
+		int x2 = input.nextInt();
+		System.out.println("Which column would you like to hit?");
+		int y2 = input.nextInt();
 		checkValid(x2, y2);
 		if(checkValid(x2,y2) == true){
+			if(EventHunterAndJonathan.AIboard2[x2][y2].equals( " X ")){
+				System.out.println("You have already shot this location.");
+			}
 			EventHunterAndJonathan.AIboard1[x2][y2] = " X ";
 		}
 	}
 	
 	private static boolean checkValid(int x2, int y2) {
-//		if(){
-//			return true;
-//		}
+		if(x2 < EventHunterAndJonathan.AIboard1.length && y2 < EventHunterAndJonathan.AIboard1[0].length){
+			return true;
+		}
+		System.out.println("This is not a valid input.");
 		return false;
 	}
 
@@ -43,11 +88,45 @@ public class JonathanBattleShip {
 		return false;
 	}
 
+	 private static void placeShip() {
+		 Scanner input = new Scanner(System.in);
+		 System.out.println("Where would you like to place your first ship?");
+			int x = input.nextInt();
+			int y = input.nextInt();
+			EventHunterAndJonathan.board1[x][y] = " O ";
+			System.out.println("Where would you like to place your second ship?");
+			int x1 = input.nextInt();
+			int y1 = input.nextInt();
+			EventHunterAndJonathan.board1[x1][y1] = " O ";
+			System.out.println("Where would you like to place your third ship?");
+			int x2 = input.nextInt();
+			int y2 = input.nextInt();
+			EventHunterAndJonathan.board1[x2][y2] = " O ";
+	}
+	 
 	public static boolean AIwin() {
-		//make it so that it tells how many ships player has left
+		//make it so that it know how many ships player has left
 		if(numberOfPlayerShips == 0){
 			return true;
 		}
 		return false;
 	}
+	
+//	public static void print(int roomsX, int roomsY) {
+//		 EventHunterAndJonathan.map = " ";
+//			for(int i = 0; i < EventHunterAndJonathan.board1[0].length + 1;i++){
+//				EventHunterAndJonathan.map += "___";//4 underscores
+//			}
+//			EventHunterAndJonathan.map += "___\n";//3 underscores
+//			//fix map!
+//			for(CaveRoom[] row: CaveExplorer.caves){
+//				//three rows of text
+//				for(int i = 0; i < 3; i++){
+//					//a line of text for each
+//					String text = "";
+//					text += "|";
+//					EventHunterAndJonathan.map += text + "\n";
+//				}// last of the 3 text lines
+//			}//last row
+//}
 }

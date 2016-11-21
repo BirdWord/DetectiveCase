18f8ed1b7618965f3e6cbc4ad92a48279e71dbee
Kristy <Kristy@kristys-mbp.home>
2016-11-21 00:50:30 -0500

    key

--- Detective/src/DavidAndKristy/FoundKitchenKey.java
+++ Detective/src/DavidAndKristy/FoundKitchenKey.java
@@ -5,11 +5,11 @@ import caveExplorer.Event;
 import caveExplorer.GameStartEvent;
 
 public class FoundKitchenKey implements Event {
-	private static final String[] story1 = {"",
-											""
+	private static final String[] story1 = {"Ouch! What did I step on? Woah, it's a black key!",
+											"Maybe it's the missing key to unlock the door!"
 											};
-	private static final String[] story2 = {"",
-											""};
+	private static final String[] story2 = {"As you pick up the key, you get a sudden memory flash.",
+											"Why did I just have a vision about Christmas in Times Square?"};
 	@Override
 	public void play() {
 		GameStartEvent.readSequence(story1);

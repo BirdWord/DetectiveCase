8126dba081b4ab63e7d6d097e2e1a8708a1b0afe
klo8715 <klo8715@bths.edu>
2016-11-20 19:58:01 -0500

    hi

--- Detective/src/packageKristyandRay/Kristy.java
+++ Detective/src/packageKristyandRay/Kristy.java
@@ -1,12 +1,14 @@
 package packageKristyandRay;
 
-import java.util.Scanner;
+import java.lang.reflect.Array;
+
+import caveExplorer.CaveExplorer;
+//import java.util.Scanner;
 
 public class Kristy {
 
-	static Scanner in;
 	public static void connect4(String[][] arr){
-		in = new Scanner(System.in);
+//		in = new Scanner(System.in);
 		for(int i = 0; i<arr.length; i++){
 			for(int j = 0; j<arr[0].length; j++){
 				arr[i][j] = " ";
@@ -15,7 +17,9 @@ public class Kristy {
 		while(true){
 			RayGUInWIN.printBoard(arr);
 			System.out.println("Which column?");
-			int input = Integer.parseInt(in.nextLine());
+//			int ing = Integer.parseInt(in.nextLine());
+//			String ing = CaveExplorer.in.nextLine();
+			int input =getIntegerInput();
 			
 			if(input <= arr[0].length-1 && input > -1){
 				int row = arr.length-1;
@@ -28,14 +32,37 @@ public class Kristy {
 				}
 				if(row > -1){
 					arr[row][input] = "o";
-					RayGUInWIN.determineWinner(arr,row,input);
-					aiTurn(arr);
+					if(RayGUInWIN.determineIfWinner(arr,row,input))
+					{
+						RayGUInWIN.determineWinner(arr,row,input);
+						break;
+					}
+					aiTurn(arr);//hi
+					
+					
 				}
 			}
 			else
 				System.out.println("That is not a valid column number.");
 		}
 	}
+	private static int getIntegerInput() {
+		 System.out.println("Please enter an integer.");
+		 boolean isInteger = false;
+		 String integerString = CaveExplorer.in.nextLine();
+		 int value = 0;
+		 while(!isInteger){
+		 try{
+		 value = Integer.parseInt(integerString);
+		 //will not continue if an error above is thrown
+		 isInteger = true;//exits loop if entry is valid
+		 }catch(NumberFormatException e){
+		 System.out.println("You must enter an integer. You better try again.");
+		 integerString = CaveExplorer.in.nextLine();
+		 }
+		 }
+		 return value;
+		 }
 	public static void aiTurn(String[][] arr)
 	{
 			double rand = Math.random();
@@ -48,10 +75,22 @@ public class Kristy {
 					{
 						arr[i][roll]="x";
 						i =-1;
+						if(RayGUInWIN.determineIfWinner(arr,i,roll))
+						{
+							RayGUInWIN.determineWinner(arr,i,roll);
+							break;
+						}
 					
 					}
-				}
 				
+				}
+//		while(!arr[0][roll].equals(null)){
+//			
+//			arr[row][roll] = "x";
+//		}
+			//ji
 	}
+	
+	
 
 }

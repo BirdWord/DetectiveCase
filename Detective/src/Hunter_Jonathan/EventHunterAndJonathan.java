066e15c212c6e18be2b954c56b5e5a4fa356aa79
johnz_000 <johnz_000@192.168.1.8>
2016-11-21 00:01:50 -0500

    uh..needs fixing. Hunter, you can fix too.

--- Detective/src/Hunter_Jonathan/EventHunterAndJonathan.java
+++ Detective/src/Hunter_Jonathan/EventHunterAndJonathan.java
@@ -2,17 +2,25 @@ package Hunter_Jonathan;
 
 import java.util.Scanner;
 
+import caveExplorer.CaveExplorer;
+import caveExplorer.CaveRoom;
+
 public class EventHunterAndJonathan {
 
 	//radars are odd
 	//ships are even
 	public static String[][] board1;
 	public static String[][] AIboard1;
+	public static String[][] board2;
+	public static String[][] AIboard2;
+	private static String map;
 	
 	public EventHunterAndJonathan() {
 		Scanner input = new Scanner(System.in);
 		board1 = new String[6][6];
 		AIboard1 = new String[6][6];
+		board2 = new String[6][6];
+		AIboard2 = new String[6][6];
 		int shotsHit = 0;
 		int tries = 0;
 		
@@ -62,26 +70,19 @@ public class EventHunterAndJonathan {
 	}
 
 	 public static void print(int roomsX, int roomsY) {
-			for (int j = 0; j != roomsX; j++) {
-				System.out.print("____");
+		 map = " ";
+			for(int i = 0; i < board1[0].length + 1;i++){
+				map += "___";//4 underscores
 			}
-			System.out.println();
-			
-			for (int i = 0; i != roomsY; i++) {
-				for (int j = 3; j > 0; j--) {
-					for (int k = 0; k != roomsX; k++) {
-						if (j == 1) {
-							System.out.print("|___");
-						} else {
-							if (j == 2) {
-								System.out.print("|   ");
-							} else {
-								System.out.print("|   ");
-							}
-						}
-					}
-					System.out.println("|");
-				}
-			}
-		}
+			map += "___\n";//3 underscores
+			for(CaveRoom[] row: CaveExplorer.caves){
+				//three rows of text
+				for(int i = 0; i < 3; i++){
+					//a line of text for each
+					String text = "";
+					text += "|";
+					map += text + "\n";
+				}// last of the 3 text lines
+			}//last row
+}
 }

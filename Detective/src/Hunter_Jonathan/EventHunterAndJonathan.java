6d0b41941488377d8b63c5524119b5007d6c2f1c
johnz_000 <johnz_000@192.168.1.8>
2016-11-20 22:53:30 -0500

    more random stuff. i honestly dk what i'm doing

--- Detective/src/Hunter_Jonathan/EventHunterAndJonathan.java
+++ Detective/src/Hunter_Jonathan/EventHunterAndJonathan.java
@@ -7,43 +7,50 @@ public class EventHunterAndJonathan {
 	//radars are odd
 	//ships are even
 	public static int[][] board1;
-	public static int[][] board2;
 	public static int[][] AIboard1;
-	public static int[][] AIboard2;
 	private String map;
 	
 	public EventHunterAndJonathan() {
 		Scanner input = new Scanner(System.in);
 		board1 = new int[6][6];
-		board2 = new int[6][6];
 		AIboard1 = new int[6][6];
-		AIboard2 = new int[6][6];
 		int[][] ships = new int[3][2];
 		int[] shot = new int[2];
-		int tries = 0;
 		int shotsHit = 0;
-		
 		
 		System.out.println("Welcome to the game of Battleship.");
 		printBoard(board1);
-		System.out.println("Where would you like to place your first ship?");
-		int x = input.nextInt();
-		int y = input.nextInt();
-		placeShip(x,y);
-		System.out.println("Where would you like to place your second ship?");
-		int x1 = input.nextInt();
-		int y1 = input.nextInt();
-		placeShip(x1,y1);
-		System.out.println("Where would you like to place your third ship?");
-		int x2 = input.nextInt();
-		int y2 = input.nextInt();
-		placeShip(x2,y2);
-		
-		
+		placeShip();
+		while(!JonathanBattleShip.win()){
+			printBoard(board1);
+			if(input.equals("ship")){
+				System.out.println("You have won the game.");
+			}
+			System.out.println("Which row would you like to hit?");
+			int x = input.nextInt();
+			System.out.println("Which column would you like to hit?");
+			int y = input.nextInt();
+			JonathanBattleShip.attack(x,y);
+			if(JonathanBattleShip.hit() == true){
+				shotsHit++;
+			}
+		}
 	}
 
-	 private void placeShip(int x, int y) {
-		
+	 private void placeShip() {
+		 Scanner input = new Scanner(System.in);
+		 System.out.println("Where would you like to place your first ship?");
+			int x = input.nextInt();
+			int y = input.nextInt();
+			//board1[x][y] = " O ";
+			System.out.println("Where would you like to place your second ship?");
+			int x1 = input.nextInt();
+			int y1 = input.nextInt();
+			//board1[x1][y1] = " O ";
+			System.out.println("Where would you like to place your third ship?");
+			int x2 = input.nextInt();
+			int y2 = input.nextInt();
+			//board1[x2][y2] = " O ";
 	}
 
 	public static void printBoard(int[][] board){
--- Detective/src/Hunter_Jonathan/JonathanBattleShip.java
+++ Detective/src/Hunter_Jonathan/JonathanBattleShip.java
@@ -1,18 +1,29 @@
 package Hunter_Jonathan;
 
-public class JonathanBattleShip {
+import java.util.Scanner;
 
-	public static int[] attack(int x, int y){
-		
-		return null;
+public class JonathanBattleShip {
+	
+	public static boolean win(){
+		int tries = 0;
+		int shotsHit = 0;
+		if(shotsHit == 3){
+			return true;
+		}
+		return false;
+	}
+
+	public static void attack(int x2, int y2){
+		//EventHunterAndJonathan.AIboard1[x2][y2] = " X ";
 	}
 	
-	
-	public static void hit(int[] shot, int[][] board){
-		for(int hit = 0; hit < board.length; hit++){
-		}	
-	}
-	public static void printBoards(){
-		
+	public static boolean hit() {
+		for(int row = 0; row < EventHunterAndJonathan.AIboard1.length; row++){
+			for(int col = 0; col < EventHunterAndJonathan.AIboard1[0].length; col++){
+				//if(EventHunterAndJonathan.AIboard1[row][col] = true){
+					
+				}
+			}
+		return false;
 	}
 }

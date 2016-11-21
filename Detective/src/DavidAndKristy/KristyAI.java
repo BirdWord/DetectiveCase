58e3583a9ecb76bd508bf9d29354c4cf6c9ff16b
Kristy <Kristy@kristys-mbp.home>
2016-11-20 21:49:28 -0500

    DavidDotsBoxes.grid

--- Detective/src/DavidAndKristy/KristyAI.java
+++ Detective/src/DavidAndKristy/KristyAI.java
@@ -8,10 +8,9 @@ public class KristyAI {
 	public static int computerBox;
 	public static boolean doubleCrossed;
 	public static boolean moreBoxes;
-	public static String[][] grid = EventDavidAndKristy.grid; // save reference from the main grid.
 	
 	public static void main(String[] args){ //wont make a box if it need a vertical line in the last column 
-		setGrid(3,3);
+		
 //		grid[0][0] = "o--";	
 //		grid[1][1] = "|  ";
 //		grid[2][0] = "o--";
@@ -30,7 +29,7 @@ public class KristyAI {
 //		grid[4][0] = "o--";
 		
 		
-		printGrid();
+		DavidDotsBoxes.printGrid();
 		computerTurn();
 	}
 	
@@ -43,14 +42,14 @@ public class KristyAI {
 				x = (int) (Math.random() * 7);  //change it to user input for number of rows and 
 				y = (int) (Math.random() * 4);  //change it to user input for number of columns
 				
-				if(x % 2 == 0 && !grid[x][y].equals("o--") && y != 3){ //change the 3 to user input 
-					grid[x][y] = "o--";
-					printGrid();  
+				if(x % 2 == 0 && !DavidDotsBoxes.grid[x][y].equals("o--") && y != 3){ //change the 3 to user input 
+					DavidDotsBoxes.grid[x][y] = "o--";
+					DavidDotsBoxes.printGrid();  
 					inLoop = false;
 				}
-				else if(x % 2 == 1 && !grid[x][y].equals("|  ")){
-					grid[x][y] = "|  ";
-					printGrid();
+				else if(x % 2 == 1 && !DavidDotsBoxes.grid[x][y].equals("|  ")){
+					DavidDotsBoxes.grid[x][y] = "|  ";
+					DavidDotsBoxes.printGrid();
 					inLoop = false;
 				}
 			}
@@ -61,14 +60,14 @@ public class KristyAI {
 					y = (int) (Math.random() * 4);  //change it to user input for number of columns
 					System.out.println("x = " + x);
 					System.out.println("y = " + y);
-					if(x % 2 == 0 && !grid[x][y].equals("o--") && y != 3){ //change the 3 to user input
-						grid[x][y] = "o--";
-						printGrid();
+					if(x % 2 == 0 && !DavidDotsBoxes.grid[x][y].equals("o--") && y != 3){ //change the 3 to user input
+						DavidDotsBoxes.grid[x][y] = "o--";
+						DavidDotsBoxes.printGrid();
 						inLoop = false;
 					}
-					else if(x % 2 == 1 && !grid[x][y].equals("|  ")){
-						grid[x][y] = "|  ";
-						printGrid();
+					else if(x % 2 == 1 && !DavidDotsBoxes.grid[x][y].equals("|  ")){
+						DavidDotsBoxes.grid[x][y] = "|  ";
+						DavidDotsBoxes.printGrid();
 						inLoop = false;
 					}
 				}
@@ -79,29 +78,33 @@ public class KristyAI {
 	public static void checkBoxes(){
 		moreBoxes = true;
 		while(moreBoxes){
-			for(int row = 0; row < grid.length -1; row++){ // it wont check the last row of the grid because there is no more rows under it
-				for(int col = 0; col < grid[0].length; col++){
+			for(int row = 0; row < DavidDotsBoxes.grid.length -1; row++){ // it wont check the last row of the grid because there is no more rows under it
+				for(int col = 0; col < DavidDotsBoxes.grid[0].length; col++){
 					DavidDotsBoxes.addScore(DavidDotsBoxes.whoseTurn()); // computer's score is updated.
-					if(grid[row][col].equals("o--") && grid[row + 1][col].equals("|  ") && grid[row + 2][col].equals("o--") && grid[row + 1][col + 1].equals(" ")){
-						grid[row + 1][col + 1] = "|  ";
+					if(DavidDotsBoxes.grid[row][col].equals("o--") && DavidDotsBoxes.grid[row + 1][col].equals("|  ") && 
+							DavidDotsBoxes.grid[row + 2][col].equals("o--") && DavidDotsBoxes.grid[row + 1][col + 1].equals(" ")){
+						DavidDotsBoxes.grid[row + 1][col + 1] = "|  ";
 						computerBox++;
 	//					System.out.println("a");
 						moreBoxes = true; 
 					}
-					else if(grid[row][col].equals("o--") && grid[row + 1][col].equals("|  ") && grid[row + 1][col + 1].equals("|  ") && grid[row + 2][col].equals("o  ")){
-						grid[row + 2][col] = "o--";
+					else if(DavidDotsBoxes.grid[row][col].equals("o--") && DavidDotsBoxes.grid[row + 1][col].equals("|  ") && 
+							DavidDotsBoxes.grid[row + 1][col + 1].equals("|  ") && DavidDotsBoxes.grid[row + 2][col].equals("o  ")){
+						DavidDotsBoxes.grid[row + 2][col] = "o--";
 						computerBox++;
 	//					System.out.println("b");
 						moreBoxes = true;
 					}
-					else if(grid[row][col].equals("|  ") && grid[row + 1][col].equals("o--") && grid[row][col + 1].equals("|  ") && grid[row-1][col].equals("o  ")){
-						grid[row - 1][col] = "o--";
+					else if(DavidDotsBoxes.grid[row][col].equals("|  ") && DavidDotsBoxes.grid[row + 1][col].equals("o--") && 
+							DavidDotsBoxes.grid[row][col + 1].equals("|  ") && DavidDotsBoxes.grid[row-1][col].equals("o  ")){
+						DavidDotsBoxes.grid[row - 1][col] = "o--";
 						computerBox++;
 	//					System.out.println("c");
 						moreBoxes = true;
 					}
-					else if(grid[row][col].equals("o--") && grid[row + 1][col + 1].equals("|  ") && grid[row + 2][col].equals("o--") && grid[row + 1][col].equals("   ")){
-						grid[row + 1][col] = "|  ";
+					else if(DavidDotsBoxes.grid[row][col].equals("o--") && DavidDotsBoxes.grid[row + 1][col + 1].equals("|  ") && 
+							DavidDotsBoxes.grid[row + 2][col].equals("o--") && DavidDotsBoxes.grid[row + 1][col].equals("   ")){
+						DavidDotsBoxes.grid[row + 1][col] = "|  ";
 						computerBox++;
 	//					System.out.println("d");
 						moreBoxes = true;
@@ -120,35 +123,39 @@ public class KristyAI {
 	
 	public static void doubleCross(){ //simplified version of double crossed
 		boolean crossOnce = false;
-		for(int row = 0; row < grid.length - 1; row++){
-			for(int col = 0; col < grid[0].length; col++){
-				if(!crossOnce && grid[row][col].equals("o  ") && grid[row + 1][col].equals("|  ") && grid[row + 1][col + 1].equals("|  ") &&
-						grid[row + 3][col].equals("|  ") && grid[row + 3][col + 1].equals("|  ") && grid[row + 4][col].equals("o--") &&
-						grid[row + 2][col].equals("o  ")){
-					grid[row][col] = "o--";
+		for(int row = 0; row < DavidDotsBoxes.grid.length - 1; row++){
+			for(int col = 0; col < DavidDotsBoxes.grid[0].length; col++){
+				if(!crossOnce && DavidDotsBoxes.grid[row][col].equals("o  ") && DavidDotsBoxes.grid[row + 1][col].equals("|  ") && 
+						DavidDotsBoxes.grid[row + 1][col + 1].equals("|  ") && DavidDotsBoxes.grid[row + 3][col].equals("|  ") && 
+						DavidDotsBoxes.grid[row + 3][col + 1].equals("|  ") && DavidDotsBoxes.grid[row + 4][col].equals("o--") &&
+						DavidDotsBoxes.grid[row + 2][col].equals("o  ")){
+					DavidDotsBoxes.grid[row][col] = "o--";
 					doubleCrossed = true;
 					crossOnce = true;
 						
 				}
-				else if(!crossOnce && grid[row][col].equals("o--") && grid[row + 1][col].equals("|  ") && grid[row + 1][col + 1].equals("|  ") &&
-						grid[row + 3][col].equals("|  ") && grid[row + 3][col + 1].equals("|  ") && grid[row + 4][col].equals("o  ") &&
-						grid[row + 2][col].equals("o  ")){
-					grid[row + 4][col] = "o--";
+				else if(!crossOnce && DavidDotsBoxes.grid[row][col].equals("o--") && DavidDotsBoxes.grid[row + 1][col].equals("|  ") && 
+						DavidDotsBoxes.grid[row + 1][col + 1].equals("|  ") && DavidDotsBoxes.grid[row + 3][col].equals("|  ") && 
+						DavidDotsBoxes.grid[row + 3][col + 1].equals("|  ") && DavidDotsBoxes.grid[row + 4][col].equals("o  ") &&
+						DavidDotsBoxes.grid[row + 2][col].equals("o  ")){
+					DavidDotsBoxes.grid[row + 4][col] = "o--";
 					doubleCrossed = true;
 					crossOnce = true;
 						
 				}
-				else if(!crossOnce && grid[row][col].equals("o--") && grid[row][col + 1].equals("o--") && grid[row + 1][col + 1].equals("   ") &&
-						grid[row + 1][col + 2].equals("|  ") && grid[row + 2][col].equals("o--") && grid[row + 2][col + 1].equals("o--") &&
-						grid[row + 1][col].equals("   ")){
-					grid[row + 1][col] = "|   ";
+				else if(!crossOnce && DavidDotsBoxes.grid[row][col].equals("o--") && DavidDotsBoxes.grid[row][col + 1].equals("o--") && 
+						DavidDotsBoxes.grid[row + 1][col + 1].equals("   ") && DavidDotsBoxes.grid[row + 1][col + 2].equals("|  ") && 
+						DavidDotsBoxes.grid[row + 2][col].equals("o--") && DavidDotsBoxes.grid[row + 2][col + 1].equals("o--") &&
+						DavidDotsBoxes.grid[row + 1][col].equals("   ")){
+					DavidDotsBoxes.grid[row + 1][col] = "|   ";
 					doubleCrossed = true;
 					crossOnce = true;
 				}
-				else if(!crossOnce && grid[row][col].equals("o--") && grid[row][col + 1].equals("o--") && grid[row + 1][col + 1].equals("   ") &&
-						grid[row + 1][col + 2].equals("   ") && grid[row + 2][col].equals("o--") && grid[row + 2][col + 1].equals("o--") &&
-						grid[row + 1][col].equals("|  ")){
-					grid[row + 1][col + 2] = "|  ";
+				else if(!crossOnce && DavidDotsBoxes.grid[row][col].equals("o--") && DavidDotsBoxes.grid[row][col + 1].equals("o--") && 
+						DavidDotsBoxes.grid[row + 1][col + 1].equals("   ") && DavidDotsBoxes.grid[row + 1][col + 2].equals("   ") && 
+						DavidDotsBoxes.grid[row + 2][col].equals("o--") && DavidDotsBoxes.grid[row + 2][col + 1].equals("o--") &&
+						DavidDotsBoxes.grid[row + 1][col].equals("|  ")){
+					DavidDotsBoxes.grid[row + 1][col + 2] = "|  ";
 					doubleCrossed = true;
 					crossOnce = true;
 				}
@@ -165,39 +172,4 @@ public class KristyAI {
 //			}
 //	}
 	
-	
-	public static void setGrid(int row, int col){
-		// to connect two points, we'll use 'o--' instead of 'o', IF horizontal.
-		grid = new String[(2 * (row + 1)) - 1][col + 1];
-		String token; // string that will be given to column.
-		for(int y = 0; y < grid.length; ++y){ // traverse through the rows.
-			/*
-			*  When creating lines, there are Vertical and Horizontal lines.
-			*  Horizontal lines will be easy to make and are nothing special.
-			*  But vertical lines are different, since they require a String Line of its.
-			*  We will draw both lines by dividing the rows by two types.
-			*
-			*  The rows will be divided into two types -> even or odd.
-			*  Even -> the dots will be drawn.
-			*  Odd -> space will be allocated for future VERTICAL lines.
-			*
-			*  A token will be used to give the column the value needed.
-			*  token -> if even: regular row with dot. if odd: special row that will be used to allocate space.
-			* */
-			if(y % 2 == 0) token = "o  "; // even row.
-			else token = "   "; // odd row.
-			for(int x = 0; x < grid[y].length; ++x){ // traverse through the column.
-				grid[y][x] = token; // set the value to the token based on the type of row.
-			}
-		}
-	}
-	public static void printGrid(){
-		// procedure to print the grid array.
-		for(int y = 0; y < grid.length; ++y){
-			for(int x = 0; x < grid[y].length; ++x){
-				System.out.print(grid[y][x]);
-			}
-			System.out.println(""); // every row needs a new line.
-		}
-	}
 }

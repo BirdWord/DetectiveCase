21ef0f00171db1fdb2387e124be9ec4065d05121
Hunter <Hunter@Hunter-HP>
2016-11-21 00:47:34 -0500

    random ships

--- Detective/src/Hunter_Jonathan/HunterAI.java
+++ Detective/src/Hunter_Jonathan/HunterAI.java
@@ -3,17 +3,27 @@ package Hunter_Jonathan;
 public class HunterAI {
 
 	public HunterAI() {
+		for(int i = 0; i < EventHunterAndJonathan.AIboard1.length; i++){
+			for(int j = 0; j < EventHunterAndJonathan.AIboard1[0].length; j++){
+				EventHunterAndJonathan.AIboard1[i][j] = " ";
+			}
+		}
+		for(int i = 0; i < EventHunterAndJonathan.AIboard2.length; i++){
+			for(int j = 0; j < EventHunterAndJonathan.AIboard2[0].length; j++){
+				EventHunterAndJonathan.AIboard2[i][j] = " ";
+			}
+		}
 		generateShips();
 	}
 
-	public void generateShips() {
+	public static void generateShips() {
 		int ships = 4;
 		while (ships > 1){
-			int shipRow = (int)(Math.random() * EventHunterAndJonathan.AIboard1.length);
-			int shipCol = (int)(Math.random() * EventHunterAndJonathan.AIboard1[0].length);
+			int shipRow = (int)(Math.random() * EventHunterAndJonathan.AIboard2.length);
+			int shipCol = (int)(Math.random() * EventHunterAndJonathan.AIboard2[0].length);
 			if(valid(shipRow, shipCol,ships)){
 				for(int i = 0; i < ships;i++){
-					EventHunterAndJonathan.AIboard1[shipRow + i][shipCol + i] = "O";
+					EventHunterAndJonathan.AIboard2[shipRow][shipCol + i] = "O";
 				}
 				ships --;
 			}
@@ -21,11 +31,11 @@ public class HunterAI {
 	}
 
 	public static boolean valid(int row, int col, int ship){
-		if(col + ship > EventHunterAndJonathan.AIboard1[0].length){
+		if(col + ship > EventHunterAndJonathan.AIboard2[0].length){
 			return false;
 		}
-		for(int i = 0; i < ship;i++){
-			if (EventHunterAndJonathan.AIboard1[row+i][col+i].equals("O")){
+		for(int i = 0; i < ship-1;i++){
+			if (EventHunterAndJonathan.AIboard2[row][col+i].equals("O")){
 				return false;
 			}
 		}
